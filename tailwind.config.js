2ecd2b8c2c1fd2e2b9b57184450b1ed368e12f90
Nishant Kumar Singh <nishant220902@gmail.com>
2022-08-06 17:38:56 +0530

    Made facebook login page clone using tailwind css (not responsive)

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        'parrot-green': '#42b72a',
+        'parrot-green-dark': '#3ba626'
+      },
+    },
+  },
+  plugins: [],
+}
